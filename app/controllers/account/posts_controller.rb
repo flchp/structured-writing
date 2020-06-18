a8ef3769b03ecb572ba3428b47c08c86918c488b
flchp <574947038@qq.com>
2020-06-18 21:48:33 +0800

    implement account/posts#CRUD

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,55 @@
+class Account::PostsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to account_posts_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "post deleted"
+    redirect_to account_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+
+  namespace :account do
+    resources :posts
+  end
+
 end
